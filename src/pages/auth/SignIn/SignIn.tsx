7c5bbe7e83e13e4bb9e92ee453bb8f6c04482fce
DeadDPie <elizabethfox434@gmail.com>
2024-11-06 13:56:58 +0700

    took out input

--- src/pages/auth/SignIn/SignIn.tsx
+++ src/pages/auth/SignIn/SignIn.tsx
@@ -1,93 +1,77 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import { ResendCodeText } from "@pages/auth/SignIn/ResendCodeText.tsx";
 
-import { SignInSchemaType, signInSchema } from "@shared/types/types.ts";
+import {
+  SignInSchemaType,
+  signInSchemaWithOtp,
+  signInSchemaWithoutOtp,
+} from "@shared/types/types.ts";
 import { Button } from "@shared/ui/Button/Button.tsx";
+import { Input } from "@shared/ui/Input/Input.tsx";
 import { Typography } from "@shared/ui/Typography/Typography.tsx";
 
 export const SignIn = () => {
+  const [otp, setOtp] = useState<boolean>(false);
   const {
     register,
     handleSubmit,
-    setValue,
-    watch,
     formState: { errors },
   } = useForm<SignInSchemaType>({
     mode: "onChange",
-    resolver: zodResolver(signInSchema),
-    defaultValues: { hasOtp: false },
+    resolver: zodResolver(otp ? signInSchemaWithOtp : signInSchemaWithoutOtp),
   });
-  const hasOtp = watch("hasOtp");
 
   const onSubmit: SubmitHandler<SignInSchemaType> = (data) => {
-    console.log(data, hasOtp);
-    if (!hasOtp) {
-      setValue("hasOtp", true);
+    console.log(data, otp);
+    if (!otp) {
+      setOtp(true);
     } else {
       console.log("Вы вошли в аккаунт");
     }
   };
-  console.log(hasOtp);
   return (
     <div className="flex flex-col items-start mt-24 ml-60 gap-6">
       <Typography variant="t">Вход</Typography>
       <Typography variant="p16">
-        Введите {hasOtp ? "проверочный код" : "номер телефона"} для входа <br />
-        в личный кабинет
+        Введите {otp ? "проверочный код" : "номер телефона"} для входа <br />в
+        личный кабинет
       </Typography>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-4 w-full max-w-[464px]"
       >
-        <input
+        <Input
+          name="phone"
+          register={register}
+          errors={errors.phone}
           placeholder="Телефон"
-          {...register("phone", { required: true })}
+          isRequired={true}
           onInput={(e) => {
             e.currentTarget.value = e.currentTarget.value.replace(/\D/g, "");
           }}
-          className={`flex items-start p-3 gap-2  w-full 
-             border rounded-lg 
-             bg-white box-border 
-             mt-1 mb-1
-        ${errors.phone ? "border-red-500" : "border-gray"} 
-        bg-gray-100 transition-all duration-200 ease-out`}
         />
-        {errors.phone && (
-          <p className="text-red-500 mt-1">{errors.phone.message}</p>
-        )}
-
-        {hasOtp && (
-          <>
-            <input
-              placeholder="Проверочный код"
-              {...register("code", { required: hasOtp })} //{ required: hasOtp }
-              onInput={(e) => {
-                e.currentTarget.value = e.currentTarget.value.replace(
-                  /\D/g,
-                  "",
-                );
-              }}
-              className={`flex items-start p-3 gap-2 w-full 
-               border rounded-lg 
-               bg-white box-border 
-               mt-1 mb-1
-          ${errors.code ? "border-red-500" : "border-gray"} 
-          bg-gray-100 transition-all duration-200 ease-out`}
-            />
-            {errors.code && (
-              <p className="text-red-500 mt-1">{errors.code.message}</p>
-            )}
-          </>
+        {otp && (
+          <Input
+            name="code"
+            register={register}
+            errors={errors.code}
+            placeholder="Проверочный код"
+            isRequired={otp}
+            onInput={(e) => {
+              e.currentTarget.value = e.currentTarget.value.replace(/\D/g, "");
+            }}
+          />
         )}
 
         <Button type="submit" variant={"FILL"} className={"max-w-[328px]"}>
-          {hasOtp ? "Войти" : "Продолжить"}
+          {otp ? "Войти" : "Продолжить"}
         </Button>
       </form>
 
-      {hasOtp && <ResendCodeText />}
+      {otp && <ResendCodeText />}
     </div>
   );
 };
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,14 +1,18 @@
 import { z } from "zod";
 
-export const signInSchema = z.object({
+export const signInSchemaWithoutOtp = z.object({
+  phone: z.string().min(1, { message: "Поле является обязательным" }),
+});
+
+export const signInSchemaWithOtp = z.object({
   phone: z.string().min(1, { message: "Поле является обязательным" }),
   code: z
     .string()
-    .optional()
-    .refine((val) => !val || val.length === 6, {
-      message: "Код должен содержать 6 цифр",
-    }),
-  hasOtp: z.boolean(),
+    .min(6, { message: "Код должен содержать 6 цифр" })
+    .max(6, { message: "Код должен содержать 6 цифр" }),
 });
 
-export type SignInSchemaType = z.infer<typeof signInSchema>;
+export type SignInSchemaType = {
+  phone: string;
+  code?: string;
+};
